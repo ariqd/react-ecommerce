f0529f8e88ba3d68a947f75490ba9807ea5a6e6b
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-03-19 11:14:53 +0700

    Header and Homepage Banner

---
+++ src/components/Directory/index.js
@@ -0,0 +1,30 @@
+import "./styles.scss";
+import shopMens from "../../assets/shopMens.jpg";
+import shopWomen from "../../assets/shopWomens.jpg";
+
+const Directory = (props) => {
+  return (
+    <div className="directory">
+      <div className="wrap">
+        <div
+          className="item"
+          style={{
+            backgroundImage: `url(${shopWomen})`,
+          }}
+        >
+          <a href="">Shop Womens</a>
+        </div>
+        <div
+          className="item"
+          style={{
+            backgroundImage: `url(${shopMens})`,
+          }}
+        >
+          <a href="">Shop Mens</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Directory;
